804a3b23d2521e5111a2c304f9d9f69572776628
Rob <robcalcote@gmail.com>
2018-11-09 19:30:28 -0700

    changed RouteConfig to pass values from URL to View

--- BlowOut/App_Start/RouteConfig.cs
+++ BlowOut/App_Start/RouteConfig.cs
@@ -13,6 +13,14 @@ namespace BlowOut
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+
+            // This route takes parameters as part of the URL string OR as a query and passes the values back to the user
+            routes.MapRoute(
+                name: "Email",
+                url: "Contact/Email/{name}/{email}",
+                defaults: new { controller = "Contact", action = "Email", name = UrlParameter.Optional, email = UrlParameter.Optional}
+            );
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ BlowOut/Controllers/ContactController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlowOut.Controllers
+{
+    public class ContactController : Controller
+    {
+        public string sName = null;
+        public string sEmail = null;
+
+        // GET: Contact
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Email(string name, string email)
+        {
+            this.sName = name;
+            this.sEmail = email;
+
+            ViewBag.vbName = sName + ".";
+            ViewBag.vbEmail = sEmail;
+
+            return View();
+        }
+    }
+}
